d2b386ea98ef4215aa64d96a149dcbd95e78519a
Adri <laslegoaventuras@gmail.com>
2025-03-22 16:45:04 -0600

    Rebuild with new caché file

--- MPOINTERS1/cmake_install.cmake
+++ MPOINTERS1/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/lasle/Desktop/o-o/U/I Semestre 2025/Datos2/MPOINTERS1/MPOINTERS1
+# Install script for directory: C:/Users/lasle/Desktop/o-o/U/I Semestre 2025/Datos2/Proyecto-I-Datos-II/MPOINTERS1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/lasle/Desktop/o-o/U/I Semestre 2025/Datos2/MPOINTERS1/MPOINTERS1/install_local_manifest.txt"
+  file(WRITE "C:/Users/lasle/Desktop/o-o/U/I Semestre 2025/Datos2/Proyecto-I-Datos-II/MPOINTERS1/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/lasle/Desktop/o-o/U/I Semestre 2025/Datos2/MPOINTERS1/MPOINTERS1/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/lasle/Desktop/o-o/U/I Semestre 2025/Datos2/Proyecto-I-Datos-II/MPOINTERS1/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
